5808d0c731261f12ba7bc8e5a0cea21039396bc5
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-05-01 21:15:37 -0500

    funcs

--- src/make_fs_rois.sh
+++ src/make_fs_rois.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 ### EXTRACT CORTICAL REGIONS FROM FREESURFER DKT ATLAS
+### MAKE COMBINED FS_MASKS
+### MAKE AVOID MASKS
 
 source functions.sh
 
@@ -8,6 +10,7 @@ source functions.sh
 roi_dir=testdir
 roi_niigz=aparc.DKTatlas+aseg
 
+cd "${roi_dir}"
 
 # Get single-ROI masks for the needed ROIs
 for v in \
@@ -38,50 +41,75 @@ for v in \
 1006 1007 1009 1015 1016 \
 2006 2007 2009 2015 2016 \
 ; do
-	split_roi "${roi_dir}" "${roi_niigz}" $v
+	split_roi "${roi_niigz}" $v
 done
 
 
 
-# Re-join into the needed masks
+# Re-join into the needed single-ROI masks
 
-join_rois "${roi_dir}" "${roi_niigz}"   FS_MOTOR_L     "1003 1017 1024"
-join_rois "${roi_dir}" "${roi_niigz}"   FS_MOTOR_R     "2003 2017 2024"
+join_rois "${roi_niigz}"   FS_MOTOR_L     "1003 1017 1024"
+join_rois "${roi_niigz}"   FS_MOTOR_R     "2003 2017 2024"
 
-join_rois "${roi_dir}" "${roi_niigz}"   FS_OCC_L       "1005 1011 1013 1021"
-join_rois "${roi_dir}" "${roi_niigz}"   FS_OCC_R       "2005 2011 2013 2021"
+join_rois "${roi_niigz}"   FS_OCC_L       "1005 1011 1013 1021"
+join_rois "${roi_niigz}"   FS_OCC_R       "2005 2011 2013 2021"
 
-join_rois "${roi_dir}" "${roi_niigz}"   FS_PFC_L       "1002 1012 1014 1018 1019 1020 1026 1027 1028"
-join_rois "${roi_dir}" "${roi_niigz}"   FS_PFC_R       "2002 2012 2014 2018 2019 2020 2026 2027 2028"
+join_rois "${roi_niigz}"   FS_PFC_L       "1002 1012 1014 1018 1019 1020 1026 1027 1028"
+join_rois "${roi_niigz}"   FS_PFC_R       "2002 2012 2014 2018 2019 2020 2026 2027 2028"
 
-join_rois "${roi_dir}" "${roi_niigz}"   FS_POSTPAR_L   "1008 1025 1029 1010 1023 1031"
-join_rois "${roi_dir}" "${roi_niigz}"   FS_POSTPAR_R   "2008 2025 2029 2010 2023 2031"
+join_rois "${roi_niigz}"   FS_POSTPAR_L   "1008 1025 1029 1010 1023 1031"
+join_rois "${roi_niigz}"   FS_POSTPAR_R   "2008 2025 2029 2010 2023 2031"
 
-join_rois "${roi_dir}" "${roi_niigz}"   FS_SOMATO_L    "1022"
-join_rois "${roi_dir}" "${roi_niigz}"   FS_SOMATO_R    "2022"
+join_rois "${roi_niigz}"   FS_SOMATO_L    "1022"
+join_rois "${roi_niigz}"   FS_SOMATO_R    "2022"
 
-join_rois "${roi_dir}" "${roi_niigz}"   FS_TEMP_L      "1006 1007 1009 1015 1016 1030 1034"
-join_rois "${roi_dir}" "${roi_niigz}"   FS_TEMP_R      "2006 2007 2009 2015 2016 2030 2034"
+join_rois "${roi_niigz}"   FS_TEMP_L      "1006 1007 1009 1015 1016 1030 1034"
+join_rois "${roi_niigz}"   FS_TEMP_R      "2006 2007 2009 2015 2016 2030 2034"
 
-join_rois "${roi_dir}" "${roi_niigz}"   FS_MOFC_L      "1012 1014 1028"
-join_rois "${roi_dir}" "${roi_niigz}"   FS_MOFC_R      "2012 2014 2028"
+join_rois "${roi_niigz}"   FS_MOFC_L      "1012 1014 1028"
+join_rois "${roi_niigz}"   FS_MOFC_R      "2012 2014 2028"
 
-join_rois "${roi_dir}" "${roi_niigz}"   FS_LPFC_L      "1018 1019 1020 1027"
-join_rois "${roi_dir}" "${roi_niigz}"   FS_LPFC_R      "2018 2019 2020 2027"
+join_rois "${roi_niigz}"   FS_LPFC_L      "1018 1019 1020 1027"
+join_rois "${roi_niigz}"   FS_LPFC_R      "2018 2019 2020 2027"
 
-join_rois "${roi_dir}" "${roi_niigz}"   FS_ACC_L       "1002 1026"
-join_rois "${roi_dir}" "${roi_niigz}"   FS_ACC_R       "2002 2026"
+join_rois "${roi_niigz}"   FS_ACC_L       "1002 1026"
+join_rois "${roi_niigz}"   FS_ACC_R       "2002 2026"
 
-join_rois "${roi_dir}" "${roi_niigz}"   FS_PPC_L       "1008 1025 1029"
-join_rois "${roi_dir}" "${roi_niigz}"   FS_PPC_R       "2008 2025 2029"
+join_rois "${roi_niigz}"   FS_PPC_L       "1008 1025 1029"
+join_rois "${roi_niigz}"   FS_PPC_R       "2008 2025 2029"
 
-join_rois "${roi_dir}" "${roi_niigz}"   FS_PARDMN_L    "1010 1031"
-join_rois "${roi_dir}" "${roi_niigz}"   FS_PARDMN_R    "2010 2031"
+join_rois "${roi_niigz}"   FS_PARDMN_L    "1010 1031"
+join_rois "${roi_niigz}"   FS_PARDMN_R    "2010 2031"
 
-join_rois "${roi_dir}" "${roi_niigz}"   FS_AUD_L       "1030 1034"
-join_rois "${roi_dir}" "${roi_niigz}"   FS_AUD_R       "2030 2034"
+join_rois "${roi_niigz}"   FS_AUD_L       "1030 1034"
+join_rois "${roi_niigz}"   FS_AUD_R       "2030 2034"
 
-join_rois "${roi_dir}" "${roi_niigz}"   FS_ITEMP_L     "1006 1007 1009 1015 1016"
-join_rois "${roi_dir}" "${roi_niigz}"   FS_ITEMP_R     "2006 2007 2009 2015 2016"
+join_rois "${roi_niigz}"   FS_ITEMP_L     "1006 1007 1009 1015 1016"
+join_rois "${roi_niigz}"   FS_ITEMP_R     "2006 2007 2009 2015 2016"
+
+
+# Create 6- and 10-ROI single-image sets
+
+# 6-ROI
+fslmaths FS_PFC_L     -add FS_PFC_R       -mul 1             tmp
+fslmaths FS_MOTOR_L   -add FS_MOTOR_R     -mul 2  -add tmp   tmp
+fslmaths FS_SOMATO_L  -add FS_SOMATO_R    -mul 3  -add tmp   tmp
+fslmaths FS_POSTPAR_L -add FS_POSTPAR_R   -mul 4  -add tmp   tmp
+fslmaths FS_OCC_L      -add FS_OCC_R      -mul 5  -add tmp   tmp
+fslmaths FS_TEMP_L    -add FS_TEMP_R      -mul 6  -add tmp   tmp
+mv tmp.nii.gz FS_6MASKS.nii.gz
+
+# 10-ROI
+fslmaths FS_MOFC_L    -add FS_MOFC_R     -mul 1              tmp
+fslmaths FS_LPFC_L    -add FS_LPFC_R     -mul 2   -add tmp   tmp
+fslmaths FS_ACC_L     -add FS_ACC_R      -mul 3   -add tmp   tmp
+fslmaths FS_PPC_L     -add FS_PPC_R      -mul 4   -add tmp   tmp
+fslmaths FS_PARDMN_L  -add FS_PARDMN_R   -mul 5   -add tmp   tmp
+fslmaths FS_AUD_L     -add FS_AUD_R      -mul 6   -add tmp   tmp
+fslmaths FS_ITEMP_L   -add FS_ITEMP_R    -mul 7   -add tmp   tmp
+fslmaths FS_MOTOR_L   -add FS_MOTOR_R    -mul 8   -add tmp   tmp
+fslmaths FS_SOMATO_L  -add FS_SOMATO_R   -mul 9   -add tmp   tmp
+fslmaths FS_OCC_L     -add FS_OCC_R      -mul 10  -add tmp   tmp
+mv tmp.nii.gz FS_10MASKS.nii.gz
 
 
---
+++ src/pipeline.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Main pipeline
+
+
+### EXTRACT CORTICAL REGIONS FROM FREESURFER DKT ATLAS
+make_fs_rois.sh
+
