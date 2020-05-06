d780e099232e281457916540a400145381bb2412
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-05-06 10:18:52 -0500

    cleanup

--- src/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source functions.sh
-
-for v in 1003 1017 1024 ; do
-	split_roi testdir aparc.DKTatlas+aseg.nii.gz $v
-done
-
-
-join_rois testdir aparc.DKTatlas+aseg.nii.gz roi.nii.gz "1003 1017 1024"
